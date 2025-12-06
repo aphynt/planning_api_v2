b17bcdef9a5c0a035145be17d4c8aa996ed00c82
aphynt <ahmadfadillah502@gmail.com>
2025-12-06 20:49:37 +0800

    update

--- app/Http/Controllers/SOPController.php
+++ app/Http/Controllers/SOPController.php
@@ -46,7 +46,7 @@ class SOPController extends Controller
             ], 404);
         }
 
-        $fileUrl = "http://127.0.0.1:8003/sop/{$file->FILE}";
+        $fileUrl = "http://36.67.119.214:9011/sop/{$file->FILE}";
 
         return response()->json([
             'success' => true,
